60a8fc5d9c2f029d550a1aa35cd0a1b2fa1bb1c5
joseph ikinda <josephikinda@gmail.com>
2024-03-04 05:48:51 +0100

    navbar anderung

--- app/Components/autres/Navbar.tsx
+++ app/Components/autres/Navbar.tsx
@@ -21,22 +21,18 @@ function Navbar() {
   return (
     <nav className='flex  top-0 z-50  sticky bg-[#F49F0A] items-center px-[20px] py-[16px] justify-between lg lg:mx-auto lg:container'>
       <div className='flex gap-x-4'>
-        <p className=' font-black text-2xl text-monoir'>Joseph Ikinda</p>
+        <p className=' font-black text-2xl text-white'>Joseph Ikinda</p>
         <div className='hidden lg:flex pl-[74px] gap-x-[56px] items-center'>
           {navItems.map((item, index)=>(
             <Link key={index} href={item.link}>
-              <p className='font-medium text-monoir' key={index}>{item.name}</p>
+              <p className='font-medium text-white' key={index}>{item.name}</p>
             </Link>
           ))}
         </div>
       </div>
       {/* Menu handy */}
       <div className='flex gap-x-3  justify-between'>
-          <div className='hidden lg:flex gap-x-4 items-center'>
-            {iconItems.map((item, index)=>(
-              <item.name key={index}  className=' h-5 w-5'/>
-            ))}
-          </div>
+         
               <DropdownMenu>
                 <DropdownMenuTrigger>
                     <MenuIcon className='lg:hidden h-5 w-5'/>
